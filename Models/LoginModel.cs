9d0e85b957119d73b94b293f38516b1d40f76ded
Francisco Lopez <wkm.jtirado@terceiros.mag.com.br>
2025-01-11 02:42:53 -0300

    feat: validation for login

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,8 +1,13 @@
-﻿namespace APIRest.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIRest.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El Email ingresado no es válido.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "El campo Password es obligatorio.")]
         public string Password { get; set; }
     }
 }
